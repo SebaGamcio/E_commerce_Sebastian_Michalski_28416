052674f197877d6a6a6d673c047911d398f05777
unknown <sebamichalski99@gmail.com>
2020-06-19 12:33:41 +0200

    koniec

--- myshop/README.md
+++ myshop/README.md
@@ -1,6 +1,6 @@
 # Myshop
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.1.7.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.1.1.
 
 ## Development server
 
--- myshop/src/app/app.module.ts
+++ myshop/src/app/app.module.ts
@@ -1,29 +1,48 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import {RouterModule, Routes} from '@angular/router';
-
-const appRoutes: Routes = [
-  { path: 'products', component: ProductsComponent},
-  { path: 'landing', component: LandingComponent},
-  { path: '', redirectTo: '/landing', pathMatch: 'full'}
-];
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { RouterModule, Routes } from '@angular/router';
 import { LandingComponent } from './components/landing/landing.component';
 import { ProductsComponent } from './components/products/products.component';
+import { PanelTopComponent } from './components/panel-top/panel-top.component';
+import { CartComponent } from './components/cart/cart.component';
+import { DashboardComponent } from './components/admin/dashboard/dashboard.component';
+import { MenuComponent } from './components/admin/menu/menu.component';
+import { AdmProductsComponent } from './components/admin/adm-products/adm-products.component';
+import { ChartsModule } from 'ng2-charts';
+import { OrdersComponent } from './components/admin/orders/orders.component';
+
+const appRoutes: Routes = [
+  { path: 'cart', component: CartComponent },
+  { path: 'products', component: ProductsComponent },
+  { path: 'landing', component: LandingComponent },
+  { path: 'adm_dashboard', component: DashboardComponent },
+  { path: 'adm_products', component: AdmProductsComponent },
+  { path: '', redirectTo: '/landing', pathMatch: 'full' },
+  {path: 'adm_orders', component: OrdersComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingComponent,
-    ProductsComponent
+    ProductsComponent,
+    PanelTopComponent,
+    CartComponent,
+    DashboardComponent,
+    MenuComponent,
+    AdmProductsComponent,
+    OrdersComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
     RouterModule.forRoot(appRoutes),
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
